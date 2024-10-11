bcf8d9309b92b05f26a186cc488f3fbb208bbf5e
Mark Keeble <mjkeeble@yahoo.de>
2024-10-11 12:33:12 +0200

    refactor repertoire component for JSON-server

--- src/components/Repertoire/Repertoire.tsx
+++ src/components/Repertoire/Repertoire.tsx
@@ -1,24 +1,35 @@
-import { storeSetlist } from '@context/index';
+import { storeSetlist, storeSongs } from '@context/index';
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { NavIndicator, SongListButton } from '..';
-import songs from '../../../data/songs.json';
-import { footswitch } from '../../const';
-import { TSong } from '../../types';
+import { BREAK, footswitch } from '../../const';
+import {TSong} from '../../types';
+import { fetchSongs } from './utils';
+import {fetchAndStoreSongs} from '@components/Setlist/utils';
 
 const Repertoire = () => {
   const navigate = useNavigate();
   const buttonsRef = useRef<HTMLButtonElement[]>([]);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [repertoireList, setRepertoireList] = useState<TSong[]>([]);
 
   // TODO: at the moment setlist is stored in local storage and
 
-  const repertoireList: TSong[] = songs
-    .sort((a, b) => a.title.localeCompare(b.title));
-  const songIds: number[] = songs.map((song: TSong) => song.id);
 
   useEffect(() => {
+    const getAndSetSongs = async () => {
+      const songList = await fetchSongs();
+      if(songList) {
+        setRepertoireList(songList);
+      }
+    }
+  
+    getAndSetSongs()
+  }, [])
+  
+  useEffect(() => {
     storeSetlist([0]);
+    storeSongs([]);
     if (buttonsRef.current[0]) {
       buttonsRef.current[0].focus();
     }
@@ -59,7 +70,6 @@ const Repertoire = () => {
           endOfListRef.current.scrollIntoView({ behavior: 'smooth' });
         }
       } else if (event.key === footswitch.leftLong) {
-
         if (document.exitFullscreen) {
           document.exitFullscreen();
         }
@@ -71,13 +81,14 @@ const Repertoire = () => {
     let storageUpdateDebounce: NodeJS.Timeout | null = null;
 
     console.log('storing to setlist', id);
-    storeSetlist(['BREAK', id]);
+    storeSetlist([BREAK, Number(id)]);
+    fetchAndStoreSongs([id]);
     if (storageUpdateDebounce) clearTimeout(storageUpdateDebounce);
     storageUpdateDebounce = setTimeout(() => {
       console.log('storing to setlist', id);
 
       navigate(`/song/1`);
-    }, 1000);
+    }, 3000);
   };
 
   return (
@@ -85,15 +96,14 @@ const Repertoire = () => {
       <div onKeyDown={handleKeyDown} tabIndex={0}>
         <h1 className="my-5 font-fredericka text-7xl text-bj-white">Repertoire</h1>
         <ul className="mb-20 mt-8">
-          {songIds.map((songId: number, index) => {
-            const song: TSong | undefined = songs.find((song) => song.id === songId);
-            if (!song) return null;
+          {repertoireList.map((song: TSong, index) => {
+            
 
             return (
               <li key={index}>
                 <SongListButton
                   ref={(el: HTMLButtonElement) => (buttonsRef.current[index] = el)}
-                  classes=""
+                  classes="text-bj-white bg-bj-blue-dark"
                   onclick={() => handleSelectSong(song.id)}
                   title={song.title}
                   version={song.version}
