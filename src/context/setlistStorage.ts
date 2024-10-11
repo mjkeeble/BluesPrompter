dea4bd0202247d3b62e1a124397f8fd5a00677d6
Mark Keeble <mjkeeble@yahoo.de>
2024-10-11 10:54:46 +0200

    refactor context funtions and local storage

--- src/context/setlistStorage.ts
+++ src/context/setlistStorage.ts
@@ -1,14 +1,12 @@
-import { TSetlist } from 'src/types';
+import {TSetlist} from 'src/types';
+
+const LOCAL_STORAGE_KEY = 'SETLIST';
 
 export const storeSetlist = (setlist: TSetlist) => {
-  window.sessionStorage.setItem('SETLIST', JSON.stringify(setlist));
-};
-
-export const deleteSetlist = () => {
-  window.sessionStorage.removeItem('SETLIST');
+  window.sessionStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(setlist));
 };
 
 export const getSetlist = (): TSetlist  => {
-  const setlist = window.sessionStorage.getItem('SETLIST');
+  const setlist = window.sessionStorage.getItem(LOCAL_STORAGE_KEY);
   return setlist ? JSON.parse(setlist) : [];
 }
